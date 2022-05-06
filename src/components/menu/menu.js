86041349cac9338de639f84f1a7da5a53173291b
Ali <aasaidi54@gmail.com>
2022-05-06 19:44:44 +0200

    Minor commit Minor commit

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -15,33 +15,6 @@ export class Menu extends React.Component {
   }
 
   mainMenu() {
-    return (
-      <div id="main-menu">
-        <div className="main-menu-item hoverable">
-          <p>1</p>
-          {/* <div>
-            <h5>games stuff</h5>
-          </div> */}
-        </div>
-        <div className="main-menu-item hoverable">
-          <p>2</p>
-          {/* <div>
-            <h5>commercial</h5>
-          </div> */}
-        </div>
-        <div className="main-menu-item hoverable">
-          <p>3</p>
-          {/* <div>
-            <h5>2d/3d art</h5>
-          </div> */}
-        </div>
-        <div className="main-menu-item hoverable">
-          <p>4</p>
-          {/* <div>
-            <h5>audiovisual</h5>
-          </div> */}
-        </div>
-      </div>
-    );
+    return <div id="main-menu"></div>;
   }
 }
