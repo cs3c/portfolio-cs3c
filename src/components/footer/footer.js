2a1a74cf630b800b6f4b7b2264bcd4127390598d
Ali <48242964+cs3c@users.noreply.github.com>
2023-03-28 00:08:52 +0000

    Minor changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,6 @@ export class Footer extends React.Component {
       <span>
         <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
           <FaReact className="footer-icon" />
-          REACT
         </a>
       </span>
     </p>
@@ -20,12 +19,12 @@ export class Footer extends React.Component {
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/3/33/Parental_Advisory_label.svg"
         alt="Parental Advisory: Explicit Content"
-        width="100px"
+        width="75px"
       />
       <img
         src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/774c14d7-308c-4108-aadd-487ec52dbf3d/d58vitj-c2d7ad24-4e5f-4e2a-ada6-92f67895f6f8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzc3NGMxNGQ3LTMwOGMtNDEwOC1hYWRkLTQ4N2VjNTJkYmYzZFwvZDU4dml0ai1jMmQ3YWQyNC00ZTVmLTRlMmEtYWRhNi05MmY2Nzg5NWY2ZjgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TZsbBepkmpSp__XpzZXUGR6t-e_mZr9o5hyVeWtOCN8"
         alt="Meme"
-        width="50px"
+        width="45px"
         style={{ marginLeft: "10px" }}
       />
     </div>
@@ -36,7 +35,7 @@ export class Footer extends React.Component {
       <footer id="footer">
         <section>
           {this.fgImages}
-          <p>MIT License @ 2022.</p>
+          <p>MIT License @ 2023.</p>
           <hr />
           {this.fgFootnote}
         </section>
